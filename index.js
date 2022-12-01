23608e6b2106e151e5609b9e0a86b228ad7dd90b
James Yang <jamesdaeyang@gmail.com>
2022-12-01 19:05:10 +0000

    Finished all features

--- index.js
+++ index.js
@@ -6,24 +6,48 @@ fs.readFile('data.json', 'utf8', (err, data) => {
     console.error(err);
     process.exit(1);
   }
-  if (process.argv[2] === 'read') {
-    var numbers =Object.keys(obj.notes);
-    for (let i=0; i<numbers.length;i++){
-    console.log(numbers[i]+ ': '+obj.notes[i+1]);
-  }
-  }
-  if (process.argv[2]==='create'){
-
-    let note = process.argv[3];
-    obj.notes[obj.nextId] = note;
-    obj.nextId++;
-    let newObj = JSON.stringify(obj, null, 2);
-    fs.writeFile('data.json', newObj , 'json', err => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
+  switch (process.argv[2]) {
+    case 'read':
+      let numbers = Object.keys(obj.notes);
+      let values = Object.values(obj.notes);
+      for (let i = 0; i < numbers.length; i++) {
+        console.log(numbers[i] + ': ' + values[i]);
+      }
+      break;
+    case 'create':
+      let note = process.argv[3];
+      obj.notes[obj.nextId] = note;
+      obj.nextId++;
+      let newObj = JSON.stringify(obj, null, 2);
+      fs.writeFile('data.json', newObj, 'utf8', err => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      })
+      break;
+    case 'update':
+      note = process.argv[4];
+      obj.notes[process.argv[3]] = note;
+      newObj = JSON.stringify(obj, null, 2);
+      fs.writeFile('data.json', newObj, 'utf8', err => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      });
+      break;
+    case 'delete':
+      delete obj.notes[process.argv[3]];
+      newObj = JSON.stringify(obj, null, 2);
+      fs.writeFile('data.json', newObj, 'utf8', err => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      });
+      break;
+    default:
+      break;
     }
-    });
-
-  }
-})
+  });
